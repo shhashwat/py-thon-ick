95cce0bd054a50185f184759bbab95100b97d149
Shashwat Singh <sshashwatssingh@gmail.com>
2025-06-02 14:26:21 +0530

    python done

--- josephus.py
+++ josephus.py
@@ -1,14 +1,20 @@
-def findTheWinner(n: int, k: int) -> int:
-        def josephus(n: int)->int:
-            if n == 1: return 0
-            return (josephus(n-1) + k)%n
-        return josephus(n) + 1
+# def findTheWinner(n: int, k: int) -> int:
+#         def josephus(n: int)->int:
+#             if n == 1: return 0
+#             return (josephus(n-1) + k)%n
+#         return josephus(n) + 1
+
+def findTheWinner(n: int, k:int)->int:
+      s: int = 0
+      for i in range(2, n+1):
+            s = (s+k)%i
+      return s + 1
 
 def main()->None:
-      n: int = int(input())
-      k: int = int(input())
+      n: int = int(input("Enter the size of the circle: "))
+      k: int = int(input("Enter the counting number: "))
 
-      print(findTheWinner(n,k))
+      print(f"The safe position is: {findTheWinner(n,k)}")
 
 if __name__ == "__main__":
       main()
